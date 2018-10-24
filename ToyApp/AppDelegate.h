e0c9489cc715f2fb12f6956c8b45012d9f755885
Joshua <joshuaxcode@grannysmith.local>
2018-10-24 20:41:06 -0230

    Initial Commit

---
+++ ToyApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ToyApp
+//
+//  Created by Joshua on 2018-10-24.
+//  Copyright © 2018 jtc260. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
